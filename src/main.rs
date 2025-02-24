5cc7e5663696fbe157f3bfa7063edfc5d45d5056
Edgar Modesto <edgarmodesto23@gmail.com>
2025-02-23 18:27:53 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    // match r {
-    //     Ok(val) => print!(""),
-    //     Err(err) => {
-    //         print!("An error ocurred while reading input");
-    //     }
-    // }
+    let r = stdin.read_line(&mut input);
+    match r {
+        Ok(val) => print!("{val}: command not found"),
+        Err(err) => {
+            print!("An error ocurred while reading input: {err}");
+        }
+    }
 }
