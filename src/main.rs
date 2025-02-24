c80354c2ccc0b89ef21667983cf5b7f7be54e97c
Edgar Modesto <edgarmodesto23@gmail.com>
2025-02-23 18:26:35 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    // match r {
+    //     Ok(val) => print!(""),
+    //     Err(err) => {
+    //         print!("An error ocurred while reading input");
+    //     }
+    // }
 }
